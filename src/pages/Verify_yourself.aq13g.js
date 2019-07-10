30c706c8d010f0d1706e36dc4a75d6bc87952969
topnotchbiz <topnotchmembers@outlook.com>
2019-07-10 09:51:57 +0200

    update

--- src/pages/Verify_yourself.aq13g.js
+++
@@ -1,8 +0,0 @@
-import wixLocation from 'wix-location';
-
-$w.onReady(function () {
-  $w("#dataset1").onAfterSave((itemBeforeSave, itemAfterSave) => {
-    console.log(itemAfterSave);
-    wixLocation.to("https://binance.facekom.net/" + itemAfterSave['_id']);
-  });
-});
